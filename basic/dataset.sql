473da56a15ca64e82bfcd1fcfe4773bfd9dd04c8
Silly_coder <104496990+Pritam-25@users.noreply.github.com>
2026-03-17 17:52:27 +0530

    Add SQL scripts to create orders table and insert sample data

--- basic/dataset.sql
+++ basic/dataset.sql
@@ -18,3 +18,21 @@ VALUES
     ('Georg', 'UK', 750),
     ('Martin', 'Germany', 500),
     ('Peter', 'USA', 0);
+
+-- create the orders table
+CREATE TABLE orders (
+    order_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE,
+    sales INT,
+    CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE
+);
+
+-- insert some data into the orders table
+INSERT INTO
+    orders (customer_id, order_date, sales)
+VALUES
+    (1, '2025-01-11', 35),
+    (2, '2025-04-05', 15),
+    (3, '2025-06-18', 20),
+    (4, '2025-08-31', 10);
